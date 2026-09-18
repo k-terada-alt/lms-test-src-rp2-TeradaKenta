4ff2618e9fff5e85d67e269195a649caeee79bd8
k-terada <k-terada@ISI-OA25L14>
2026-09-18 14:19:04 +0900

    add:Case02.javaログイン認証失敗
    ログイン認証失敗時のテストコードとコメントを追加
    入力値
    ログインID：wrongId
    パスワード：wrongPassword

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,10 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 /**
  * 結合テスト ログイン機能①
@@ -17,7 +22,11 @@ import org.junit.jupiter.api.TestMethodOrder;
  */
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース02 受講生 ログイン 認証失敗")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class Case02 {
+
+	@LocalServerPort
+	private int port;
 
 	/** 前処理 */
 	@BeforeAll
@@ -35,14 +44,40 @@ public class Case02 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
+		//ログイン画面のURLに遷移
+		goTo("http://localhost:" + port + "/lms/");
+
+		//タイトルが正しいか検証
+		String title = webDriver.getTitle();
+		assertEquals("ログイン | LMS", title);
+
+		//ボタンが正しいか検証
+		WebElement loginButtonElement = webDriver.findElement(By.cssSelector(".btn-primary"));
+		assertEquals("ログイン", loginButtonElement.getAttribute("value"));
+
+		//エビデンスを取得
+		getEvidence(new Object() {});
+
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに登録されていないユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+
+		//登録されていない情報を入力
+		webDriver.findElement(By.id("loginId")).sendKeys("wrongId");
+		webDriver.findElement(By.id("password")).sendKeys("wrongPassword");
+		webDriver.findElement(By.cssSelector(".btn-primary")).click();
+
+		//タイトルとエラーメッセージが正しいか検証
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		assertEquals("* ログインに失敗しました。", webDriver.findElement(By.cssSelector(".error")).getText());
+
+		//エビデンスを取得
+		getEvidence(new Object() {});
+
 	}
 
 }
