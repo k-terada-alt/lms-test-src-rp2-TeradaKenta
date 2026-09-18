f28e8b27a0ae9dfa9332d67c069330ef5c699810
k-terada <k-terada@ISI-OA25L14>
2026-09-18 11:55:26 +0900

    add:Case01.java ログイン画面への遷移テストコード追加 WebElementを利用してタイトルの値を取得、ログインIDの値とパスワードの値は入力欄の表示状態を検証、それぞれ期待値通りかどうか確認するテストを作成。 期待値は以下の通り タイトル：ログイン | LMS ログインID：表示確認 パスワード：表示確認

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 /**
  * 結合テスト ログイン機能①
@@ -17,7 +20,11 @@ import org.junit.jupiter.api.TestMethodOrder;
  */
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース01 ログイン画面への遷移")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class Case01 {
+	
+	@LocalServerPort
+	private int port;
 
 	/** 前処理 */
 	@BeforeAll
@@ -35,7 +42,15 @@ public class Case01 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		
+		goTo("http://localhost:" + port + "/lms/");
+		
+		getEvidence(new Object() {});
+		
+		String title = webDriver.getTitle();
+		
+		assertEquals("ログイン | LMS", title);
+		
 	}
 
 }
